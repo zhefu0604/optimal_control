fix bug in gradient

models/Problem_Class.py
@@ -39,9 +39,9 @@ class OptimalControl(object):
     def grad_F(self, U): 
         X, V = self.system_solve(U)
         sol = solve_ivp(self.f_adjoint, [self.time[-1], self.time[0]], [0, 0], t_eval= np.flip(self.time)) ## todo: check if we need to flip time 
-        PQ = np.flip(sol.y)
-        P = PQ[0, :]
-        Q = PQ[1, :]
+        PQ = sol.y
+        P = np.flip(PQ[0, :])
+        Q = np.flip(PQ[1, :])
         V = self.V(self.time)
         dz = -1 * Q + (self.p0 + self.p1 * V + self.p2 * V**2) + (U > 0) * (2*self.q0*U + 2*self.q1*V*U)
         return dz
@@ -124,7 +124,7 @@ class OptimalControlConvex(object):
 
     def F(self, U):
         X, V = self.system_solve(U)
-        E = V**2 + U**2
+        E = 1e-2 * V**2 + U**2
         z = self.del_t * np.sum(E)
         return z
 
@@ -132,10 +132,10 @@ class OptimalControlConvex(object):
     def grad_F(self, U): 
         X, V = self.system_solve(U)
         sol = solve_ivp(self.f_adjoint, [self.time[-1], self.time[0]], [0, 0], t_eval= np.flip(self.time)) ## todo: check if we need to flip time 
-        PQ = np.flip(sol.y)
-        P = PQ[0, :]
-        Q = PQ[1, :]
-        dz = -1 * Q + 2 * U**2
+        PQ = sol.y
+        P = np.flip(PQ[0, :])
+        Q = np.flip(PQ[1, :])
+        dz = -1 * Q + 2 * U
         return dz
 
 
@@ -156,7 +156,7 @@ class OptimalControlConvex(object):
     def f_adjoint(self, t, PQ): 
         P = PQ[0]
         P_dot = 0
-        Q_dot = 2 * self.V(t) - P
+        Q_dot = 2 * 1e-2 * self.V(t) - P
         
         return np.array([P_dot, Q_dot])
 
@@ -174,15 +174,110 @@ class OptimalControlConvex(object):
             # U = np.convolve(np.ones(200), np.ones(50)/50, mode=m));
         return np.append(U, U[-1])
 
-    def constraints(self, eps = 5, gamma = 120): ## Au <= b
+    def constraints(self, eps = 5, gamma = 120, u_max = 5, u_min = -5): ## Au <= b
         T = self.del_t * np.ones([self.d, self.d])
         T[0, :] = 0
         T = np.tril(T)
         TT = T @ T
-        A = np.concatenate((TT, -TT,  -T), axis=0)
+        A = np.concatenate((TT, -TT,  -T, np.identity(self.d), -np.identity(self.d)), axis=0)
         b = np.concatenate((self.xl - self.x0 - self.v0 * self.time - eps, 
             gamma - self.xl + self.x0 + self.v0 * self.time, 
-            self.v0 * np.ones(self.d)), axis=0)
+            self.v0 * np.ones(self.d), 
+            u_max * np.ones(self.d), 
+            -u_min * np.ones(self.d)), axis=0)
+        return A, b
+
+class OptimalControlNorm(object):
+    def __init__(self, time, x0, v0, xl, vl):
+        # Fuel model parameters
+        self.C0 = 0.245104874164936
+        self.C1 = 0.00389174029574479
+        self.C2 = 0.0
+        self.C3 = 3.30050221372029e-05
+        self.p0 = 0.014702037699570
+        self.p1 = 0.051202817419139
+        self.p2 = 0.001873784068826
+        self.q0 = 0.0
+        self.q1 = 0.018538661491842
+        self.P0 = 0
+        self.Q0 = 0
+        self.beta0 = 0.194462963040005 
+
+        self.time = time
+        self.d = len(time)
+        self.del_t = self.time[-1]/self.d
+        self.x0 = x0
+        self.v0 = v0
+        self.xl = xl
+        self.vl = vl
+        self.vl_fun = interp1d(self.time, vl, kind='linear', assume_sorted=True)
+
+
+
+    def F(self, U):
+        X, V = self.system_solve(U)
+        E = self.fuel_model(V, U) + np.norm(V - self.vl)**2
+        z = self.del_t * np.sum(E)
+        return z
+
+
+    def grad_F(self, U): 
+        X, V = self.system_solve(U)
+        sol = solve_ivp(self.f_adjoint, [self.time[-1], self.time[0]], [0, 0], t_eval= np.flip(self.time)) ## todo: check if we need to flip time 
+        PQ = sol.y
+        P = np.flip(PQ[0, :])
+        Q = np.flip(PQ[1, :])
+        V = self.V(self.time)
+        dz = -1 * Q + (self.p0 + self.p1 * V + self.p2 * V**2) + (U > 0) * (2*self.q0*U + 2*self.q1*V*U)
+        return dz
+
+
+    def system_solve(self, U):
+        self.U = interp1d(self.time, U, kind='linear', assume_sorted=True)
+        T = self.del_t * np.ones([self.d, self.d])
+        T[0, :] = 0
+        T = np.tril(T)
+        TU = np.matmul(T, U)
+        V = self.v0 * np.ones(self.d) + TU;
+        X = self.x0 * np.ones(self.d) + self.v0 * self.time + np.matmul(T, TU);
+        self.X = interp1d(self.time, X, kind='linear', assume_sorted=True)
+        self.V = interp1d(self.time, V, kind='linear', assume_sorted=True)
+
+        return X, V 
+
+
+    def f_adjoint(self, t, PQ): 
+        P = PQ[0]
+        P_dot = 0
+        Q_dot = (self.C1 + 2 * self.C2 * self.V(t) + 3 * self.C3*self.V(t)**2 + (self.p1 + 2*self.p2*self.V(t)) * self.U(t) + (self.U(t) > 0) * self.q1 * self.U(t)**2) + 2 * (self.V(t) - self.vl_fun(t))- P
+        
+        return np.array([P_dot, Q_dot])
+
+
+
+    def fuel_model(self, V, U):
+        V = np.maximum(V, 0);   
+        fc = self.C0 + self.C1*V + self.C2*V**2 + self.C3*V**3 + self.p0*U + self.p1*U*V + self.p2*U*V**2 + self.q0*np.maximum(U,0)**2 + self.q1*np.maximum(U,0)**2*V
+
+        return np.maximum(fc, self.beta0)
+
+    def initialize(self, mode = "smooth"): 
+        if mode == "smooth": 
+            U = np.diff(self.vl) / self.del_t
+            # U = np.convolve(np.ones(200), np.ones(50)/50, mode=m));
+        return np.append(U, U[-1])
+
+    def constraints(self, eps = 5, gamma = 120, u_max = 5, u_min = -5): ## Au <= b
+        T = self.del_t * np.ones([self.d, self.d])
+        T[0, :] = 0
+        T = np.tril(T)
+        TT = T @ T
+        A = np.concatenate((TT, -TT,  -T, np.identity(self.d), -np.identity(self.d)), axis=0)
+        b = np.concatenate((self.xl - self.x0 - self.v0 * self.time - eps, 
+            gamma - self.xl + self.x0 + self.v0 * self.time, 
+            self.v0 * np.ones(self.d), 
+            u_max * np.ones(self.d), 
+            -u_min * np.ones(self.d)), axis=0)
         return A, b
 
 

opt_algos/projected_gradient_descent.py
@@ -58,10 +58,14 @@ def projected_gradient_descent(model, eta, max_iterations=1e4, epsilon=1e-5,
 
     # keep track of history
     x_history = []
+    f_history = []
 
     for k in range(int(max_iterations)):
 
-        x_history.append(x_current)
+        x_history.append(x_current.tolist())
+        f = model.F(x_current)
+        f_history.append(f)
+        print("Objective value = ", f)
 
         # gradient update
         y_next = x_current - eta * grad_F(x_current)
@@ -71,13 +75,12 @@ def projected_gradient_descent(model, eta, max_iterations=1e4, epsilon=1e-5,
 
         # relative error stopping condition
         if np.linalg.norm(x_next - x_current) <= epsilon*np.linalg.norm(x_current):
-            #  if np.linalg.norm(beta_next) <= epsilon:
             break
 
         x_current = x_next
-        print("Objective value = ", model.F(x_current))
 
     print('GD finished after ' + str(k) + ' iterations')
 
-    return {'solution': x_current,
-            'x_history': x_history}
+    return {'solution': x_current.tolist(),
+            'x_history': x_history, 
+            'f_history': f_history}